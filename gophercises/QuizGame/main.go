662f586f6069290597dbbef868329369c43d211a
imoleFolio <dremkay71@gmail.com>
2024-10-24 20:27:30 +0100

    part 2 done: nowworking fine, except for 1 bug the bug manifests when the first problem is anwered correctly and enter for every other one before timer isout

--- gophercises/QuizGame/main.go
+++ gophercises/QuizGame/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
-	"gophercises/quizGame/utils"
 	"os"
+	"time"
 )
 
 func main() {
 
 	csvfile := flag.String("file", "problems.csv", "path to csv file, USAGE: -file=test.csv")
-	// limit := flag.Int("limit", 10, "The time limit for the quiz in seconds, USAGE: -limit=5")
+	limit := flag.Int("limit", 10, "The time limit for the quiz in seconds, USAGE: -limit=5")
 
 	flag.Parse()
 
@@ -22,15 +22,37 @@ func main() {
 	total := len(records)
 
 	score := 0
+	done := make(chan bool)
+	answerch := make(chan bool)
+	var user_answer string
 
+	go sleepfor(*limit, done)
+
+outerLoop:
 	for index, problem := range records {
-		score += utils.Ask(index+1, problem.que, problem.ans)
-	}
+		fmt.Printf("Problem #%v: %v = ", index+1, problem.que)
 
+		go func() {
+			fmt.Scanf("%s\n", &user_answer)
+			answerch <- true
+			if user_answer == problem.ans {
+				score++
+			}
+
+		}()
+		select {
+		case <-done:
+			fmt.Println("\ntime up!!!!")
+			break outerLoop
+		case <-answerch:
+			continue
+		}
+	}
 	fmt.Printf("You scored %v out of %v.\n", score, total)
+
 }
 
-// func sleepfor(t int, done chan bool) {
-// 	time.Sleep(time.Duration(t) * time.Second)
-// 	done <- true
-// }
+func sleepfor(t int, done chan bool) {
+	time.Sleep(time.Duration(t) * time.Second)
+	done <- true
+}
